54f586fa5dd949f853ec82cb613ce1f96949362f
NHRandel <nhrandel89@gmail.com>
2025-01-22 05:09:20 +0000

    KEN

---
+++ KEN.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class KEN extends Structure {
+
+    @Override
+    public void displayPoem(ArrayList<Words> words) {
+        Random random = new Random();
+        int i = random.nextInt(5) + 1;
+
+        System.out.print(getRandomWords(i-1))
+
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayPoem'");
+    }
+    
+}
