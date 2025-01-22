c9d1357d21bbd950b1c7f80b933dc79318ceaf49
kyle-bertrand <bertrandkyle1@gmail.com>
2025-01-22 04:31:46 +0000

    Kyle Complete

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String args[]) {
         wordsList.add(new ProfHorton());
         //1
+        //2
         //3
         wordsList.add(new Ethan());
         //5
---
+++ StudentKyle.java
@@ -0,0 +1,7 @@
+public class StudentKyle implements Words{
+    public String phrase(int syllables){
+        return "refrigerator";
+    }
+
+
+}
