d77aadd0c57af2c91e6657be43fb7c0e8e97f5a7
nyloww <katanu.mwendwa@gmail.com>
2025-01-22 04:47:06 +0000

    done

--- Katanu.java
+++ Katanu.java
@@ -1,5 +1,52 @@
+import java.util.Random;
+
 public class Katanu implements Words {
-    public String phrase(int i) {
-        return "Please Read The Naming Song ";
+    public String phrase() {
+        String[] oneSyllable = {
+            "I", "love", "this", "class", "book"
+        };
+        String[] twoSyllables = {
+            "drinking", "Robin", "enjoy", "surprise", "drawing"
+        };
+        String[] threeSyllables = {
+            "Calpico", "Katsudon", "eleven", "outer space", "jupiter"
+        };
+        String[] fourSyllables = {
+            "Water bottle", "inconsistent", "watermelon", "data structures", "filtered water"
+        };
+        String[] fiveSyllables = {
+            "electricity", "intimidating", "subterranean", "abominable", "observatory"
+        };
+
+        Random random = new Random();
+
+        int syllableCount = random.nextInt(5) + 1;
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < syllableCount; i++){
+            if (syllableCount == 1){
+                result.append(oneSyllable[random.nextInt(oneSyllable.length)]);
+            }
+            else if(syllableCount == 2){
+                result.append(twoSyllables[random.nextInt(twoSyllables.length)]);
+            }
+            else if (syllableCount == 3){
+                result.append(threeSyllables[random.nextInt(threeSyllables.length)]);
+            }
+            else if (syllableCount == 4){
+                result.append(fourSyllables[random.nextInt(fourSyllables.length)]);
+            }
+            else if (syllableCount == 5){
+                result.append(fiveSyllables[random.nextInt(fiveSyllables.length)]);
+            }
+            else {
+                return "error";
+            }
+        }
+
+        return result.toString();
     }
+
+    
 }
