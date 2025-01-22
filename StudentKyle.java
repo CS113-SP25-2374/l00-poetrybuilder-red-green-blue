335e4dae8710ddce2eacdefe03cccb48cfc96d2c
kyle-bertrand <bertrandkyle1@gmail.com>
2025-01-22 04:33:09 +0000

    Ky Complete

--- StudentKyle.java
+++ StudentKyle.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+
 public class StudentKyle implements Words{
+    
+    ArrayList<String> wordsList = new ArrayList<>();
+
+    public StudentKyle()
+    {
+    wordsList.add("Me");
+    wordsList.add("meeting");
+    wordsList.add("Tides Dance like");
+    wordsList.add("Water ebbs, flows");
+    wordsList.add("Refrigerator");
+    }
+    
     public String phrase(int syllables){
-        return "refrigerator";
+        return wordsList.get(syllables);
     }
 
 
