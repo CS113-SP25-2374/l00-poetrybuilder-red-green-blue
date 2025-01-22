e8bbc4b8cba1f00c547657d3a85bffb7513f6896
NHRandel <nhrandel89@gmail.com>
2025-01-22 04:11:56 +0000

    Noah Randel

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ public class Main {
     static ArrayList<Words> wordsList = new ArrayList<>();
     static ArrayList<Structure> structureList = new ArrayList<>();
     public static void main(String args[]) {
-        //wordsList.add(new ProfHorton());
+        wordsList.add(new ProfHorton());
         //1
-        wordsList.add(new StudentKyle());
+        //2
         //3
-        wordsList.add(new Ethan());
+        //4
         //5
         //6
         //7
---
+++ NoahRandel.java
@@ -0,0 +1,9 @@
+public class NoahRandel implements Words {
+
+    public String[] wordlist = {"to" , "upon" , "general" ,"elemental" , "unbelievable"} ;
+
+
+    public String phrase(int syllables){
+        return wordlist[syllables];
+    }
+}
